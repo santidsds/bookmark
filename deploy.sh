b4fe7ae224e0502c7b52756f96b059f9c7efd081
Santiago <santidasilva1408@gmail.com>
2022-12-24 01:14:45 -0300

    gh-pages

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:santidsds/Bookmark-Page-React-Tailwind-Site.git main:gh-pages
+git push -f git@github.com:santidsds/bookmark.git main:gh-pages
 
 cd -
